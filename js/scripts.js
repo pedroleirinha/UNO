02a5253c07629c399eadfb8d55881e30024ab2ec
Thunder <oleirinha.pedro@gmail.com>
2019-10-16 17:37:44 +0100

    First upload

---
+++ js/scripts.js
@@ -0,0 +1,537 @@
+class Card {
+    constructor(JSONCard) {
+        this.id = JSONCard.id;
+        this.category = JSONCard.descricao_categoria;
+        this.color = JSONCard.descricao_cor;
+        this.quantity = JSONCard.quantidade;
+        this.number = JSONCard.numero;
+    }
+}
+
+class Player {
+    constructor(playerNumber, name) {
+        this.playerNumber = playerNumber;
+        this.name = name;
+        this.turnPlayedCards = [];
+        this.hand = [];
+        this.unoState = false;
+        this.cardsToDraw = 0;
+    }
+    disableUno() {
+        this.unoState = false;
+    }
+    enableUno() {
+        this.unoState = true;
+    }
+    hasUno() {
+        return this.unoState === true;
+    }
+    playCard(clickedCard) {
+        let index = this.hand.findIndex(card => card.id = clickedCard.id);
+        this.hand.splice(index, 1);
+        this.turnPlayedCards.push(clickedCard);
+    }
+    addCardDeck(newCard) {
+        this.hand.push(newCard);
+    }
+    getTurnCardsNumber() {
+        return this.turnPlayedCards.length;
+    }
+    emptyPlayedCards() {
+        this.turnPlayedCards = [];
+    }
+    checkPlayerHasPlayed() {
+        if (this.turnPlayedCards.length > 0) {
+            return true;
+        } else {
+            alert("You have to play at least one card in order to finish!");
+            return false;
+        }
+    }
+    hasCardsToDraw() {
+        return this.cardsToDraw > 0;
+    }
+}
+
+class Model {
+    constructor() {
+        //List of Cards
+        this.gameOver = false;
+        this.totalNumberCards = 0;
+        this.deck = [];
+        this.playerTurn = 0;
+        this.usedCards = [];
+        this.defaultHandSize = 7;
+        this.playerList = [new Player(1, "Thunder"), new Player(2, "Shadow")];
+        this.numberOfPlayers = 2;
+        this.topCard;
+        this.currentPlayer = this.playerList[0];
+    }
+
+    transferOldCardsToDeck() {
+        this.deck = this.deck.concat(this.usedCards);
+    }
+
+    isGameOver() {
+        return this.gameOver;
+    }
+
+    getNextCurrentPlayer() {
+        return this.playerList[this.playerTurn == this.playerList.length - 1 ? 0 : this.playerTurn + 1];
+    }
+
+    changeCurrentPlayer() {
+        this.playerTurn = this.playerTurn == this.playerList.length - 1 ? 0 : this.playerTurn + 1;
+    }
+
+    getCurrentPlayer() {
+        return this.playerList[this.playerTurn];
+    }
+
+
+    getStartingHands() {
+        this.playerList.forEach((player) => {
+            for (let i = 0; i < this.defaultHandSize; i++) {
+                let newCard = this.deck[i];
+                player.addCardDeck(newCard);
+                this.deck.splice(0, 1);
+            }
+        });
+    }
+
+    getNewTopCard() {
+        let allNumbers = this.deck.filter(card => card.category === "Number");
+        this.setNewTopCard(allNumbers[allNumbers.length - 1]);
+        this.deck.splice(this.deck.findIndex(card => this.topCard.id = card.id), 1);
+        return this.topCard;
+    }
+
+    setNewTopCard(newCard) {
+        this.topCard = newCard;
+    }
+
+    shuffleDeck() {
+        let currentIndex = this.deck.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (currentIndex !== 0) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            // And swap it with the current element.
+            temporaryValue = this.deck[currentIndex];
+            this.deck[currentIndex] = this.deck[randomIndex];
+            this.deck[randomIndex] = temporaryValue;
+        }
+    }
+
+    getNewCardFromDeck() {
+        return this.deck.pop();
+    }
+
+    verifyColor(cardColor) {
+        return cardColor === this.topCard.color;
+    }
+    verifyNumber(cardNumber) {
+        return cardNumber === parseInt(this.topCard.number);
+    }
+    verifyQuantity(cardQuantity) {
+        return cardQuantity === parseInt(this.topCard.quantity);
+    }
+    verifyCategory(cardCategory) {
+        return cardCategory === this.topCard.category;
+    }
+
+    matchesLastCardProp(cardPlayedNumber, prop, val) {
+        if (cardPlayedNumber === 0) {
+            return this.topCard[prop] === val;
+        }
+        return false;
+    }
+    checkNumberCard(cardPlayedNumber, number, color) {
+        return this.verifyNumber(number) || this.matchesLastCardProp(cardPlayedNumber, "color", color);
+    }
+
+    checkChangeColorCard(cardPlayedNumber, category) {
+        return cardPlayedNumber === 0 ? true : this.verifyCategory(category);
+    }
+
+    checkGoGetCard(cardPlayedNumber, quantity, category, color) {
+        if (cardPlayedNumber === 0) {
+            //If its the first card played
+            if (quantity == 2) {
+                if (this.verifyQuantity(quantity) || this.verifyColor(color) || this.verifyCategory(category)) {
+                    return true;
+                }
+            } else {
+                return true;
+            }
+        }
+        else {
+            //If second or further card played
+            if (this.verifyQuantity(quantity) && this.verifyCategory(category)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    checkChangeDirectionCard(cardPlayedNumber, color, category) {
+        return this.verifyCategory(category) || this.matchesLastCardProp(cardPlayedNumber, "color", color);
+    }
+
+    goGetCard() {
+        return this.drawCardToPlayer(this.getNextCurrentPlayer());
+    }
+
+    drawCardToPlayer(player) {
+        let drawnCard = this.getNewCardFromDeck();
+        player.addCardDeck(drawnCard);
+        return drawnCard;
+    }
+
+    skipTurn() {
+        this.changeCurrentPlayer();
+    }
+
+    checkStopCard(cardPlayedNumber, color, category) {
+        return this.verifyCategory(category) || this.matchesLastCardProp(cardPlayedNumber, "color", color);
+    }
+
+    validateCard(drawnCard, cardPlayedNumber) {
+        let number = parseInt(drawnCard.number);
+        let category = drawnCard.category;
+        let quantity = parseInt(drawnCard.quantity);
+        let color = drawnCard.color;
+
+        switch (category) {
+            case "GoGet":
+                return this.checkGoGetCard(cardPlayedNumber, quantity, category, color)
+            case "ChangeDirection":
+                return this.checkChangeDirectionCard(cardPlayedNumber, color, category);
+            case "Number":
+                return this.checkNumberCard(cardPlayedNumber, number, color);
+            case "Stop":
+                return this.checkStopCard(cardPlayedNumber, color, category);
+            case "ChangeColor":
+                return this.checkChangeColorCard(cardPlayedNumber, category);
+        }
+        return false;
+    }
+
+    playerWon(player) {
+        return player.hand.length === 0;
+    }
+
+    removePlayedCardDeck(playedCard) {
+        let indexOfPlayedCard = this.deck.indexOf((card) => card.id = playedCard.id);
+        this.deck.splice(indexOfPlayedCard, 1);
+    }
+
+    playCard(playedCard, cardPlayedNumber) {
+        if (this.validateCard(playedCard, cardPlayedNumber)) {
+            this.getCurrentPlayer().playCard(playedCard);
+
+            this.deck.splice(Math.floor(Math.random() * this.deck.length), 0, playedCard);
+            this.topCard = playedCard;
+            console.log(this.deck);
+            return true;
+        }
+        return false;
+    }
+
+    hasFullStack() {
+        return this.deck.length > 2 * this.totalNumberCards / 3;
+    }
+    hasMediumStack() {
+        return this.deck.length <= 2 * this.totalNumberCards / 3;
+    }
+
+    hasSmallStack() {
+        return this.deck.length < this.totalNumberCards / 3;
+    }
+}
+
+class View {
+    showGameBoard() {
+        $("body").delay(500).fadeIn(2000);
+    }
+
+    removeCardHand(cardToRemove) {
+        $(cardToRemove).remove();
+    }
+
+    showColorPicker() {
+        $("#colorPallet").fadeIn();
+    }
+    hideColorPicker() {
+        $("#colorPallet").fadeOut();
+    }
+    showUnoImg(playerNumber) {
+        $(`#player${playerNumber} #unoIMG`).fadeIn();
+    }
+    hideUnoImg(playerNumber) {
+        $(`#player${playerNumber} #unoIMG`).fadeOut();
+    }
+
+    showActiveTurn(playerList, activePlayer) {
+        playerList.forEach(player => $(`#player${player.playerNumber} h1`).removeClass("activeTurn"));
+        $(`#player${activePlayer.playerNumber} h1`).addClass("activeTurn");
+    }
+
+    showDeckCard(deckHand, card) {
+        deckHand.append(card);
+    }
+
+    showTopCard(topCard) {
+        $("#topCard").replaceWith($(topCard).attr("id", "topCard"));
+
+    }
+
+    displayPlayersNames(players) {
+        players.forEach(player => {
+            $(`#player${player.playerNumber} > h1`).text(player.name);
+        })
+    }
+
+    changeTopCardClass(className) {
+        let remainingDeck = $("#remainingDeck");
+        remainingDeck.removeClass();
+        remainingDeck.addClass(`${"img"} ${className}`);
+    }
+}
+
+class Controller {
+    constructor() {
+        this.view = new View();
+        this.con = new Model();
+
+        this.setButtonClicks();
+    }
+
+    setButtonClicks() {
+        this.con.playerList.forEach(player => {
+            $(`#player${player.playerNumber} #finish`).on("click", () => {
+                this.con.transferOldCardsToDeck();
+                //After clicking in the finish Button, check if it was the correct button, if the player has already played at least 1 card and if the card played doenst have a neutral back color
+                if (player.playerNumber === this.con.getCurrentPlayer().playerNumber && player.checkPlayerHasPlayed() && this.con.topCard.color !== "neutral") {
+
+                    this.cardActionByCategory(player);
+
+                    //After, check to see if it was the last card played
+                    if (this.con.playerWon(this.con.getCurrentPlayer())) {
+
+                        $(`#player${this.con.getCurrentPlayer().playerNumber}`).addClass("victory");
+                        this.con.gameOver = true;
+                        alert(`The player ${this.con.getCurrentPlayer().name} has won`);
+                    } else {
+                        //If the game continues, change player, show new pleyer turn and empty the array of cards played by the player
+                        this.con.changeCurrentPlayer();
+                        this.view.showActiveTurn(this.con.playerList, this.con.getCurrentPlayer());
+                        player.emptyPlayedCards();
+                    }
+                    console.log(this.con.deck);
+                }
+            });
+
+            $(`#player${player.playerNumber} #uno`).on("click", () => {
+                if (player.hand.length === 1) {
+                    this.view.showUnoImg(player.playerNumber);
+                    player.enableUno();
+                } else {
+                    alert("Too early for uno, my dude");
+                    //Go fetch like 7 cards
+                }
+            });
+        });
+
+        const colorPalletDiv = document.querySelectorAll("#colorPallet > div.card");
+        colorPalletDiv.forEach(colorCard => {
+            $(colorCard).on("click", () => {
+                let colorClassName = colorCard.classList[colorCard.classList.length - 1];
+                let colorName = colorCard.classList[1].slice(0, colorClassName.length - 4);
+                this.con.topCard.color = colorName;
+                $("#topCard").removeClass("neutralBack");
+                $("#topCard").addClass(colorClassName);
+                this.view.hideColorPicker();
+            });
+        });
+
+
+
+    }
+    cardActionByCategory(player) {
+        let categoryPlayed = player.turnPlayedCards[0].category;
+        switch (categoryPlayed) {
+            case "Stop":
+            case "ChangeDirection":
+                //If the played card is a STOP or CHANGEDIRECTION than skip a turn
+                this.con.skipTurn();
+                break;
+            case "GoGet":
+                //Calculate the total cards to get from the deck
+                let numberOfCardsDraw = this.con.getCurrentPlayer().turnPlayedCards.reduce((accumulator, card) => accumulator + parseInt(card.quantity), 0);
+                this.con.getNextCurrentPlayer().cardsToDraw += numberOfCardsDraw;
+                break;
+        }
+    }
+    async getAllCards() {
+        //Fetch all cards
+        return fetch("http://localhost/getCards.php").then(response => {
+            return response.json();
+        }).then(returnedValue => {
+            //For each card return in JSON, create an instance and save it in the deck array
+            returnedValue.forEach(JsonValue => {
+                this.con.deck.push(new Card(JsonValue));
+            });
+            this.con.totalNumberCards = this.con.deck.length;
+            //Shuffle the deck
+            this.con.shuffleDeck();
+            //Draw for each player, x numbers of cards. The number is define as a variable called defaultHandSize
+            this.con.getStartingHands();
+        });
+    }
+
+    startGame() {
+        //Start by hiding the color picker
+        this.view.hideColorPicker();
+        //Fetch all the cards from the Database
+        this.getAllCards().then(() => {
+            //For each player draw
+            this.con.playerList.forEach(player => {
+                //Show hands
+                this.showInitialCards(player);
+            });
+            //Get and Show the first Top Card
+            this.view.showTopCard(this.paintCard(this.con.getNewTopCard()));
+            this.view.showGameBoard();
+        });
+
+        //Set a click listener to draw a card as the user clicks in the remaining deck
+        $("#remainingDeck").on("click", () => {
+            console.log(this.con.deck);
+            if (this.con.hasFullStack()) {
+                this.view.changeTopCardClass("fullStackDeck");
+            } else if (this.con.hasSmallStack()) {
+                this.view.changeTopCardClass("smallStackDeck");
+            } else if (this.con.hasMediumStack()) {
+                this.view.changeTopCardClass("mediumStackDeck");
+            }
+
+            let currentPlayer = this.con.getCurrentPlayer();
+
+            if (!this.con.isGameOver()) {
+                let numberCardsDrawn = 0;
+
+                do {
+                    //Show a new card from the deck
+                    this.showDrawnCard(currentPlayer, this.con.drawCardToPlayer(currentPlayer));
+                    numberCardsDrawn++;
+
+                } while (numberCardsDrawn < currentPlayer.cardsToDraw);
+
+                this.con.getCurrentPlayer().cardsToDraw = 0;
+            }
+        });
+
+        //Show whitch user is the firs to play
+        this.view.showActiveTurn(this.con.playerList, this.con.getCurrentPlayer());
+    }
+
+    showDrawnCard(player, drawnCard) {
+        //Get the HTML Div for the correspondent player zone
+        let deckHand = $(`#player${player.playerNumber} > .deckZone`)[0];
+        //Create the HTML Card with all the necessary classes and content
+        let displayedCard = this.paintCard(drawnCard);
+        //Set the click listener
+        this.defineCardClickEvent(player, displayedCard, drawnCard);
+        //Show the created card in the browser
+        this.view.showDeckCard(deckHand, displayedCard)
+    }
+
+    defineCardClickEvent(player, displayedCard, clickedCardObj) {
+        //Card Clicked
+        $(displayedCard).on("click", () => {
+            //Check if, its the correct player turn and if the last card played was not a neutralCard
+            if (player.playerNumber === this.con.getCurrentPlayer().playerNumber && this.con.topCard.color !== "neutral") {
+                //Check if the played card can be played
+                let playerTurn = player.getTurnCardsNumber();
+                let playerHasCardsToDraw = player.hasCardsToDraw()
+
+                playerTurn = playerHasCardsToDraw ? 2 : player.getTurnCardsNumber();
+
+                if (this.con.playCard(clickedCardObj, playerTurn)) {
+
+                    if (playerHasCardsToDraw) {
+                        console.log(`You have ${player.cardsToDraw} cards to draw`);
+                        this.con.getNextCurrentPlayer().cardsToDraw = player.cardsToDraw;
+                        player.cardsToDraw = 0;
+                    }
+
+                    //If the card played has a neutral color, the user has to pick a new one
+                    if (clickedCardObj.color === "neutral") {
+                        this.view.showColorPicker();
+                    }
+                    this.view.removeCardHand(displayedCard);
+                    this.view.showTopCard(displayedCard);
+                }
+            }
+            else {
+                console.log(`Invalid Card`);
+            }
+        });
+    }
+
+    showInitialCards(player) {
+        //Sort initial hand by color
+        let hand = player.hand.sort((card1, card2) => card1.color.localeCompare(card2.color));;
+
+        //Get HTML Div of the Deck Zone
+        let deckHand = $(`#player${player.playerNumber} > .deckZone`)[0];
+        //Write the player Name in the titles
+        $(`#player${player.playerNumber} > h1`).text(player.name);
+
+        //Empty the deckHand just in case
+        deckHand.innerHTML = "";
+
+        //For each card in hand, display it in the browser
+        hand.forEach(card => {
+            this.showDrawnCard(player, card);
+        })
+    }
+
+    paintCard(card) {
+        //Get the card info into seperate variables
+        let color = card.color;
+        let category = card.category;
+        let quantity = parseInt(card.quantity);
+        let number = parseInt(card.number);
+
+        //Create the basic template for a card
+        let newCard = $(`<div class="card"></div>`);
+        newCard.addClass(color + "Back");
+        newCard.addClass(category);
+        newCard.addClass(category === "ChangeDirection" || category === "ChangeColor" || category === "Stop" ? "img" : "");
+
+        if (number < 10) {
+            //If the number is lesser the 10, means its a numbered card
+            newCard.text(number);
+        } else if (quantity != 0) {
+            //If the quantity is different than 0, means its a GoGet card
+            newCard.text(`+${quantity}`);
+        }
+
+        return newCard[0];
+    }
+}
+
+class Game {
+    constructor() {
+        this.controller = new Controller();
+        this.controller.startGame();
+    }
+}
+
+$(function () {
+    window.game = new Game();
+});
---
+++ poleirinha_u.sql
@@ -0,0 +1,218 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 15-Jul-2019 às 17:02
+-- Versão do servidor: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
+
+CREATE DATABASE IF NOT EXISTS `poleirinha_u`;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `poleirinha_u`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cards`
+--
+
+CREATE TABLE IF NOT EXISTS `cards` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_categoria` int(11) NOT NULL,
+  `id_cor` int(11) NOT NULL,
+  `quantidade` int(2) NOT NULL,
+  `numero` int(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_categoria` (`id_categoria`) USING BTREE,
+  KEY `fk_cor` (`id_cor`)
+) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `cards`
+--
+
+INSERT INTO `cards` (`id`, `id_categoria`, `id_cor`, `quantidade`, `numero`) VALUES
+(1, 1, 1, 0, 0),
+(2, 1, 1, 0, 1),
+(3, 1, 1, 0, 1),
+(4, 4, 1, 2, 10),
+(5, 2, 1, 0, 10),
+(6, 4, 1, 2, 10),
+(7, 2, 1, 0, 10),
+(8, 5, 1, 0, 10),
+(9, 1, 1, 0, 10),
+(10, 1, 1, 0, 2),
+(11, 1, 1, 0, 2),
+(12, 1, 1, 0, 3),
+(13, 1, 1, 0, 3),
+(14, 1, 1, 0, 4),
+(15, 1, 1, 0, 4),
+(16, 1, 1, 0, 5),
+(17, 1, 1, 0, 5),
+(18, 1, 1, 0, 6),
+(19, 1, 1, 0, 6),
+(20, 1, 1, 0, 7),
+(21, 1, 1, 0, 7),
+(22, 1, 1, 0, 8),
+(23, 1, 1, 0, 8),
+(24, 1, 1, 0, 9),
+(25, 1, 1, 0, 9),
+(26, 1, 2, 0, 0),
+(27, 1, 2, 0, 1),
+(28, 1, 2, 0, 1),
+(29, 2, 2, 0, 10),
+(30, 4, 2, 2, 10),
+(31, 2, 2, 0, 10),
+(32, 4, 2, 2, 10),
+(33, 5, 2, 0, 10),
+(34, 5, 2, 0, 10),
+(35, 1, 2, 0, 2),
+(36, 1, 2, 0, 2),
+(37, 1, 2, 0, 3),
+(38, 1, 2, 0, 3),
+(39, 1, 2, 0, 4),
+(40, 1, 2, 0, 4),
+(41, 1, 2, 0, 5),
+(42, 1, 2, 0, 5),
+(43, 1, 2, 0, 6),
+(44, 1, 2, 0, 6),
+(45, 1, 2, 0, 7),
+(46, 1, 2, 0, 7),
+(47, 1, 2, 0, 8),
+(48, 1, 2, 0, 8),
+(49, 1, 2, 0, 9),
+(50, 1, 2, 0, 9),
+(51, 3, 5, 0, 10),
+(52, 3, 5, 0, 10),
+(53, 4, 5, 4, 10),
+(54, 4, 5, 4, 10),
+(55, 4, 5, 4, 10),
+(56, 4, 5, 4, 10),
+(57, 3, 5, 0, 10),
+(58, 3, 5, 0, 10),
+(59, 1, 3, 0, 0),
+(60, 1, 3, 0, 1),
+(61, 1, 3, 0, 1),
+(62, 2, 3, 0, 10),
+(63, 4, 3, 2, 10),
+(64, 5, 3, 0, 10),
+(65, 2, 3, 0, 10),
+(66, 4, 3, 2, 10),
+(67, 5, 3, 0, 10),
+(68, 1, 3, 0, 2),
+(69, 1, 3, 0, 2),
+(70, 1, 3, 0, 3),
+(71, 1, 3, 0, 3),
+(72, 1, 3, 0, 4),
+(73, 1, 3, 0, 4),
+(74, 1, 3, 0, 5),
+(75, 1, 3, 0, 5),
+(76, 1, 3, 0, 6),
+(77, 1, 3, 0, 6),
+(78, 1, 3, 0, 7),
+(79, 1, 3, 0, 7),
+(80, 1, 3, 0, 8),
+(81, 1, 3, 0, 8),
+(82, 1, 3, 0, 9),
+(83, 1, 3, 0, 9),
+(84, 1, 4, 0, 0),
+(85, 1, 4, 0, 1),
+(86, 1, 4, 0, 1),
+(87, 4, 4, 2, 10),
+(88, 4, 4, 2, 10),
+(89, 2, 4, 0, 10),
+(90, 2, 4, 0, 10),
+(91, 5, 4, 0, 10),
+(92, 5, 4, 0, 10),
+(93, 1, 4, 0, 2),
+(94, 1, 4, 0, 2),
+(95, 1, 4, 0, 3),
+(96, 1, 4, 0, 3),
+(97, 1, 4, 0, 4),
+(98, 1, 4, 0, 4),
+(99, 1, 4, 0, 5),
+(100, 1, 4, 0, 5),
+(101, 1, 4, 0, 6),
+(102, 1, 4, 0, 6),
+(103, 1, 4, 0, 7),
+(104, 1, 4, 0, 7),
+(105, 1, 4, 0, 8),
+(106, 1, 4, 0, 8),
+(107, 1, 4, 0, 9),
+(108, 1, 4, 0, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categorias`
+--
+
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `decricao_categoria` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `decricao_categoria`) VALUES
+(1, 'Numero'),
+(2, 'Muda Sentido'),
+(3, 'Muda Cor'),
+(4, 'Buscar'),
+(5, 'Stop');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cores`
+--
+
+CREATE TABLE IF NOT EXISTS `cores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `decricao_cor` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `cores`
+--
+
+INSERT INTO `cores` (`id`, `decricao_cor`) VALUES
+(1, 'Amarelo'),
+(2, 'Azul'),
+(3, 'Verde'),
+(4, 'Vermelho'),
+(5, 'Neutro');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `cards`
+--
+ALTER TABLE `cards`
+  ADD CONSTRAINT `fk_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_cor` FOREIGN KEY (`id_cor`) REFERENCES `cores` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
